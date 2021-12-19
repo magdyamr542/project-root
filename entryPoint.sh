d2f19614b0fa6eaf1a407e04bba0fcfb6f030d44
amr.metwally <magdyamr542@gmail.com>
2021-12-19 23:11:49 +0100

    Update the entry point

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,14 +1,14 @@
 function pr {
-    output=$(node /Users/apple/Desktop/frontend_apps/project-root/dist/index.js $@)
+    output=$(node ~/.proot/index.js $@)
     retCode=$?
-    if [[ $@ == "go" && $retCode -eq 0 ]]; then
+    if [[ ( $@ == "go" || $@ == "" ) && $retCode -eq 0 ]]; then
         # cd when go command. hide output
         cd $output
     else
         echo $output
     fi
     if [ $retCode -ne 0 ]; then
-       return $retCode
+        return $retCode
     fi
-
+    
 }

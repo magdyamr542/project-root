07725aeb35d22402004ea8c2ba8d49974dba37d2
amr.metwally <magdyamr542@gmail.com>
2021-12-19 11:33:27 +0100

    add func to the entryPoint

--- entryPoint.sh
+++ entryPoint.sh
@@ -1,11 +1,14 @@
-output=$(node /Users/apple/Desktop/frontend_apps/project-root/dist/index.js $@)
-retCode=$?
-# cd when go command
-if [[ $@ == "go" && $retCode -eq 0 ]]; then
-    
-    cd $output
-fi
-echo $output
-if [ $retCode -ne 0 ]; then
-    exit 1
-fi
+function pr {
+    output=$(node /Users/apple/Desktop/frontend_apps/project-root/dist/index.js $@)
+    retCode=$?
+    if [[ $@ == "go" && $retCode -eq 0 ]]; then
+        # cd when go command. hide output
+        cd $output
+    else
+        echo $output
+    fi
+    if [ $retCode -ne 0 ]; then
+       return $retCode
+    fi
+
+}

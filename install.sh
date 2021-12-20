2527f2a1d21c7303588c583f900b18e9649ab3b1
amr.metwally <magdyamr542@gmail.com>
2021-12-20 20:04:12 +0100

    update install command

--- install.sh
+++ install.sh
@@ -3,12 +3,4 @@ npm i
 npm run build
 npm run mkbin
 cp ./bin/index.js ./entryPoint.sh ~/.proot
-if [[ -f  ~/.bashrc ]]; then
-    echo "source ~/.proot/entryPoint.sh"  >> ~/.bashrc
-fi
-
-if [[ -f  ~/.zshrc ]]; then
-    echo "source ~/.proot/entryPoint.sh"  >> ~/.zshrc
-fi
-
-echo "The app was installed succesffully. Source your shell again and use pr help to get started"
+echo Installed. Please add this line to your .bashrc or .zshrc  "'source .proot/entryPoint.sh'"

2a7745d52fd6b373097e78b3175cee0a4954b1a0
amr.metwally <magdyamr542@gmail.com>
2022-02-26 23:21:13 +0100

    added go cmd

--- go/main.go
+++ go/main.go
@@ -7,12 +7,11 @@ import (
 )
 
 var CLI struct {
-	Add    cmd.AddCmd   `cmd:"" help:"Register the given path as the root of the project (can be relative)"` // implemented
-	Ls     cmd.LsCmd    `cmd:"" help:"List all saved project roots" aliases:"l,ls"`                          // implemented
-	Remove cmd.RmCmd    `cmd:"" help:"removes all paths which end with [suffix]" aliases:"rm"`
-	Clear  cmd.ClearCmd `cmd:"" help:"clears the database of saved projects. Will delete everything. Use with CAUTION" aliases:"clr"` // implemented
-	Go     cmd.GoCmd    `cmd:"" help:"go to the root of this project" default:"1"`
-	Purge  cmd.PurgeCmd `cmd:"" help:"delete all registered paths that no longer exist in the file system"` // implemented
+	Add   cmd.AddCmd   `cmd:"" help:"Register the given path as the root of the project (can be relative)"`
+	Ls    cmd.LsCmd    `cmd:"" help:"List all saved project roots" aliases:"l,ls"`
+	Clear cmd.ClearCmd `cmd:"" help:"clears the database of saved projects. Will delete everything. Use with CAUTION" aliases:"clr"`
+	Go    cmd.GoCmd    `cmd:"" help:"go to the root of this project" default:"1"`
+	Purge cmd.PurgeCmd `cmd:"" help:"delete all registered paths that no longer exist in the file system"`
 }
 
 func main() {

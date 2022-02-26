2b517450b4b963f9c32169b49dfa4a812c883d99
amr.metwally <magdyamr542@gmail.com>
2022-02-26 19:35:54 +0100

    added clear command

--- go/cmd/clear.go
+++ go/cmd/clear.go
@@ -1,10 +1,26 @@
 package cmd
 
+import (
+	"fmt"
+	"project-root/fs"
+)
+
 // Clearing the db
 
 type ClearCmd struct{}
 
 func (clrCmd *ClearCmd) Run() error {
-	println("TODO: implement clear db")
+	if err := clear(); err != nil {
+		return fmt.Errorf("error couldn't clear the database of saved project roots. Does file exist ?")
+	}
 	return nil
+}
+
+func clear() error {
+	// Get saved data
+	storageFile, err := fs.GetStorageFile()
+	if err != nil {
+		return err
+	}
+	return fs.WriteFile(storageFile, "", false)
 }
--- go/main.go
+++ go/main.go
@@ -10,7 +10,7 @@ var CLI struct {
 	Add    cmd.AddCmd   `cmd:"" help:"Register the given path as the root of the project (can be relative)"` // implemented
 	Ls     cmd.LsCmd    `cmd:"" help:"List all saved project roots" aliases:"l,ls"`                          // implemented
 	Remove cmd.RmCmd    `cmd:"" help:"removes all paths which end with [suffix]" aliases:"rm"`
-	Clear  cmd.ClearCmd `cmd:"" help:"clears the database of saved projects. Will delete everything. Use with CAUTION" aliases:"clr"`
+	Clear  cmd.ClearCmd `cmd:"" help:"clears the database of saved projects. Will delete everything. Use with CAUTION" aliases:"clr"` // implemented
 	Go     cmd.GoCmd    `cmd:"" help:"go to the root of this project" default:"1"`
 	Purge  cmd.PurgeCmd `cmd:"" help:"delete all registered paths that no longer exist in the file system"`
 }
